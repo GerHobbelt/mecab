eb73ad532baeaf8c62519612b4a86757cba6c947
Aki Ariga <chezou+github@gmail.com>
2018-11-24 13:15:37 +0900

    Make setup.py compatible for Windows

--- mecab/python/setup.py
+++ mecab/python/setup.py
@@ -1,21 +1,44 @@
 #!/usr/bin/env python
 
-from distutils.core import setup,Extension,os
-import string
+import os
+import subprocess
+import sys
 
-def cmd1(str):
-    return os.popen(str).readlines()[0][:-1]
+from setuptools import setup, Extension
 
-def cmd2(str):
-    return string.split (cmd1(str))
+
+def mecab_config(arg):
+	output = subprocess.check_output(["mecab-config", arg])
+
+	if not isinstance(output, str):
+		output = output.decode("utf-8")
+
+	return output.split()
+
+is_windows = os.name == 'nt'
+
+is_64bits = sys.maxsize > 2**32
+
+# Assuming Windows mecab binary, headers, dlls are put into following directory
+win_mecab_dir = r'C:\mecab'
+win_sdk_dir = win_mecab_dir
+# If you want to use official Windows binary, you can comment out following variables
+#win_mecab_dir = r'C:\Program Files{}\MeCab'.format('' if is_64bits else ' (x86)')
+#win_sdk_dir = r'{}\sdk'.format(win_mecab_dir)
+
+include_dirs = [win_sdk_dir] if is_windows else mecab-config("--inc-dir")
+library_dirs = [win_sdk_dir] if is_windows else mecab-config("--libs-only-L")
+libraries = ["libmecab"] if is_windows else mecab-config("--libs-only-l")
+data_files = [("lib\\site-packages\\", ["{}\\libmecab.dll".format(win_sdk_dir)])] if is_windows else []
 
 setup(name = "mecab-python",
-	version = cmd1("mecab-config --version"),
+	version = '0.996.1',
 	py_modules=["MeCab"],
+	data_files = data_files,
 	ext_modules = [
 		Extension("_MeCab",
 			["MeCab_wrap.cxx",],
-			include_dirs=cmd2("mecab-config --inc-dir"),
-			library_dirs=cmd2("mecab-config --libs-only-L"),
-			libraries=cmd2("mecab-config --libs-only-l"))
+			include_dirs=include_dirs,
+			library_dirs=library_dirs,
+			libraries=libraries)
 			])
