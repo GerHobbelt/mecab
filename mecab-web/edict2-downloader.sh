9ca1cc22ac21d39f2b2e20350b12e48a8b3c85c0
Alex Birch <Birch-san@users.noreply.github.com>
2019-02-27 23:16:36 +0000

    UTF8 is more space-efficient for the dictionary. start thinking about how to index.

--- mecab-web/edict2-downloader.sh
+++ mecab-web/edict2-downloader.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 wget http://ftp.monash.edu/pub/nihongo/edict2.gz
 gzip -d edict2.gz
-# UTF16 is storage-efficient for CJK text. NodeJS supports low-endian.
-iconv -f EUC-JP -t UTF-16LE edict2 > edict2.utf16le.txt
+# UTF8 is probably most storage-efficient for EDICT2, since most of the document is the Engllish definitions.
+iconv -f EUC-JP -t UTF-8 edict2 > edict2.utf8.txt
+# UTF16 is storage-efficient for CJK text generally. NodeJS supports low-endian.
+# iconv -f EUC-JP -t UTF-16LE edict2 > edict2.utf16le.txt
 # iconv -f EUC-JP -t UTF-16 edict2 > edict2.utf16.txt
 # iconv -f EUC-JP -t UCS-2 edict2 > edict2.ucs2.txt
--- mecab-web/edict2-indexer.js
+++ mecab-web/edict2-indexer.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
 const { promisify } = require('util');
@@ -8,15 +9,58 @@ let inputPath;
 if (process.argv.length >= 3) {
   inputPath = process.argv[2];
 } else {
-  inputPath = path.join(__dirname, 'edict2.utf16le.txt');
+  inputPath = path.join(__dirname, 'edict2.utf8.txt');
 }
 if (!fs.existsSync(inputPath)) {
   throw new Error(`File not found: ${inputPath}
-Usage: node edict2-indexer.js edict2.utf16le.txt`);
+Usage: node edict2-indexer.js edict2.utf8.txt`);
+}
+
+// https://stackoverflow.com/a/3759300/5257399
+String.prototype.getCodePointLength= function() {
+  return this.length-this.split(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g).length+1;
+};
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
+function getWholeCharAndI(str, i) {
+  var code = str.charCodeAt(i);
+
+  if (Number.isNaN(code)) {
+    return ''; // Position not found
+  }
+  if (code < 0xD800 || code > 0xDFFF) {
+    return [str.charAt(i), i]; // Normal character, keeping 'i' the same
+  }
+
+  // High surrogate (could change last hex to 0xDB7F to treat high private 
+  // surrogates as single characters)
+  if (0xD800 <= code && code <= 0xDBFF) {
+    if (str.length <= (i + 1)) {
+      throw 'High surrogate without following low surrogate';
+    }
+    var next = str.charCodeAt(i + 1);
+      if (0xDC00 > next || next > 0xDFFF) {
+        throw 'High surrogate without following low surrogate';
+      }
+      return [str.charAt(i) + str.charAt(i + 1), i + 1];
+  }
+  // Low surrogate (0xDC00 <= code && code <= 0xDFFF)
+  if (i === 0) {
+    throw 'Low surrogate without preceding high surrogate';
+  }
+  var prev = str.charCodeAt(i - 1);
+
+  // (could change last hex to 0xDB7F to treat high private surrogates
+  // as single characters)
+  if (0xD800 > prev || prev > 0xDBFF) {
+    throw 'Low surrogate without preceding high surrogate';
+  }
+  // Return the next character instead (and increment)
+  return [str.charAt(i + 1), i + 1];
 }
 
 async function processLineByLine() {
-  const fileStream = fs.createReadStream(inputPath, {encoding: 'utf16le'});
+  const fileStream = fs.createReadStream(inputPath, {encoding: 'utf8'});
 
   const rl = readline.createInterface({
     input: fileStream,
@@ -24,14 +68,16 @@ async function processLineByLine() {
   });
 
   const readingTrailerMatcher = /\((P|ik)\)$/
-  const tokenTrailerMatcher = /\((P|iK)\)$/
+  const tokenTrailerMatcher = /\((P|iK|ateji)\)$/
   const readingsMatcher = /^\[(.*)\]$/
   const literationMatcher = /^(.*)\((.*)\)$/
-  const utf16NewlineLen = 2;
+  const newlineBytes = 1;
   let ix = 0;
   let n = 0;
   // http://www.edrdg.org/jmdict/edict_doc.html
-  rl.on('line', (line) => {
+  // rl.on('line', (line) => {
+  for await (const line of rl) {
+    if (n++<1101) continue;
     const [formText, detailText] = line.split('/', 2);
     const [tokensText, readingsText] = formText.split(' ', 2);
     const nominalTokens = tokensText.split(';');
@@ -57,11 +103,31 @@ async function processLineByLine() {
           phonetic: tuple,
         }
       })
-      console.log(literations)
+      // console.log(literations)
       // console.log(readingTuples)
     }
-    rl.close();
-  });
+    // new Int8Array(1);
+    strippedTokens[0] = '私の𩸽'
+    console.log(strippedTokens)
+    console.log(Buffer.from(strippedTokens[0], 'utf16le'));
+    function lol(token) {
+      if (!token.length) {
+        return;
+      }
+      console.log(Buffer.from(token[0], 'utf16le'));
+      lol(token.slice(1));
+    }
+    lol(strippedTokens[0]);
+    // lol('私の𩸽');
+    // rl.close();
+    //あれ以来
+
+    // useful encoding helpers (may not be needed for NodeJS, but could be needed in browser):
+    // https://stackoverflow.com/a/24391376/5257399
+    // https://stackoverflow.com/a/14641495/5257399
+    break;
+  }
+  // });
 
 }
 
