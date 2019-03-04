ef10ded86ee3b32bbe0ea811f002a9a232736cb2
Alex Birch <Birch-san@users.noreply.github.com>
2019-03-04 00:35:29 +0000

    start refactoring into modules

---
+++ mecab-web/src/edict2/index.js
@@ -0,0 +1,84 @@
+import edictAbbr from './abbrev.js';
+
+function parseEdictMeaningSection(meaningSection) {
+}
+
+function parseEnamdictMeaningSection(meaningSection) {
+}
+
+function parseEntrySection(section) {
+  const entries = section.split(';');
+  return entries.map(parseEntry);
+}
+
+function parseTags(tagsSection) {
+  const tagMatcher = /\(([^\(\)]*)\)/g;
+  const kanji = [];
+  const reading = [];
+  const misc = [];
+  let partOfSpeechSegment;
+  let priorityEntry = false;
+  while (partOfSpeechSegment = tagMatcher.exec(tagsSection)) {
+    const tagContent = partOfSpeechSegment[1];
+    if (tagContent === 'P') {
+      priorityEntry = true;
+    } else if (tagContent in edictAbbr.kanji) {
+      kanji.push(tagContent);
+    } else if (tagContent in edictAbbr.reading) {
+      reading.push(tagContent);
+    } else {
+      misc.push(partOfSpeechSegment[1]);
+    }
+  }
+  return {
+    kanji,
+    reading,
+    misc,
+    priorityEntry,
+  }
+}
+
+function parseEntry(entry) {
+  const [whole, form, tagsSection] = /([^\(\)]*)(.*)/.exec(entry);
+  return {
+    form,
+    tags: parseTags(tagsSection),
+  };
+}
+
+function parseEdictLine(glossParser, line) {
+  const [indexSection, meaningSection] = line.split('/', 2);
+  const [headwordSection, readingSection] = indexSection.split(' ', 2);
+  
+  const headwords = parseEntrySection(headwordSection);
+
+  let readings = [];
+  const readingSectionInner = /^\[(.*)\]$/.exec(readingSection);
+  if (readingSectionInner && readingSectionInner.length > 1) {
+    readings = parseEntrySection(readingSectionInner[1]);
+  }
+
+  return {
+    headwords,
+    readings,
+    meaning: glossParser(meaningSection),
+  };
+}
+
+function edictLookup([edict2Text, enamdictText], term) {
+	const regexp = new RegExp(
+  `^(.*(^|[\\[;])${
+    regExpEscape(term)
+  }[\\]\\(; ].*)$`, 'mg');
+  const edict2Matches = (edict2Text.match(regexp) || []);
+  const enamDictMatches = (enamdictText.match(regexp) || []);
+
+  return {
+    edict2: edict2Matches.map(parseEdictLine.bind(null, parseEdictMeaningSection)),
+    enamdict: enamDictMatches.map(parseEdictLine.bind(null, parseEnamdictMeaningSection)),
+  }
+}
+
+export {
+	edictLookup,
+};
--- mecab-web/src/index.js
+++ mecab-web/src/index.js
@@ -1,4 +1,7 @@
+import { toMecabTokens, withWhitespacesSplicedBackIn, withInterWordWhitespaces } from './tokenizer/index.js';
 import { edictLookup } from './edict2/index.js';
+import { createElement, Component } from '../web_modules/preact.js';
+import htm from '../web_modules/htm.js';
 
 // const td = new TextDecoder('utf-16le');
 // .then((response)=>response.arrayBuffer())
@@ -45,6 +48,157 @@ function handleTokenClickEdict2(event) {
   }
 }
 
+function createAnalysisFragment(nodes) {
+  const fragment = document.createDocumentFragment();
+  const div = document.createElement('div');
+  div.className = 'parsed-sentence';
+  div.style.order = 1;
+  let isFirstNode = true;
+  let bufferedText = '';
+  for (const node of nodes) {
+    if (node.isWhitespace) {
+      div.insertAdjacentText('beforeend', node.token);
+      continue;
+    }
+    let span;
+    if (new URLSearchParams(window.location.search).get('jisho')) {
+      span = document.createElement('span');
+      span.className = 'token';
+    } else if (new URLSearchParams(window.location.search).get('jisho_href')) {
+      span = document.createElement('a');
+      span.className = 'token2';
+      span.href = `https://jisho.org/search/${encodeURIComponent(node.token)}`;
+      span.target = '_blank';
+    } else {
+      span = document.createElement('span');
+      span.className = 'token3';
+    }
+    span._mecabToken = node.token;
+    let isFirstSubtoken = true;
+    for (const subtoken of node.subtokens) {
+      if (subtoken.type === 'kanji') {
+        if (bufferedText) {
+          span.insertAdjacentText('beforeend', bufferedText);
+          bufferedText = '';
+        }
+        const ruby = document.createElement('ruby');
+        const rb = document.createElement('rb');
+        const rt = document.createElement('rt');
+
+        rb.textContent = subtoken.value;
+        rt.textContent = subtoken.reading;
+        ruby.appendChild(rb);
+        ruby.appendChild(rt);
+        span.appendChild(ruby);
+      } else {
+        bufferedText += subtoken.value;
+      }
+      isFirstSubtoken = false;
+    }
+    if (bufferedText) {
+      span.insertAdjacentText('beforeend', bufferedText);
+      bufferedText = '';
+    }
+    div.appendChild(span);
+    isFirstNode = false;
+  }
+  // if (bufferedText) {
+  //   div.insertAdjacentText('beforeend', bufferedText);
+  // }
+  fragment.appendChild(div);
+  return fragment;
+}
+
+function handleTokenClickJishoIframe(event) {
+  const tokenNode = event.target.className === 'token'
+  ? event.target
+  : event.target.closest('.token');
+  if (tokenNode
+    && tokenNode._mecabToken) {
+    console.log(tokenNode._mecabToken);
+    const sentenceNode = tokenNode.closest('.parsed-sentence');
+    if (!sentenceNode) {
+      throw new Error('DOM not as expected');
+    }
+    +sentenceNode.style.order;
+    Array.from(richOutputElement.children)
+    .filter((element) => element.style.order > +sentenceNode.style.order)
+    .sort((left, right) => right.style.order - left.style.order) // descending
+    .forEach((child) => {
+      child.style.order = +child.style.order+1;
+    });
+    if (!iframeElement) {
+      iframeElement = document.createElement('iframe');
+      iframeContainerElement.classList.remove('hidden');
+      iframeContainerElement.appendChild(iframeElement);
+    }
+    currentQueryElement.classList.remove('hidden');
+    currentQueryTargetElement.textContent = tokenNode._mecabToken;
+    queryContainerElement.style.order = +sentenceNode.style.order+1;
+    queryContainerElement.style.display = 'initial';
+    iframeElement.src = `https://jisho.org/search/${encodeURIComponent(tokenNode._mecabToken)}`;
+  }
+}
+
+/** @author bobince
+  * @see https://stackoverflow.com/a/3561711/5257399 */
+function regExpEscape(s) {
+    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+
+const richOutputElement = document.getElementById('richOutput');
+
+function handleFormSubmitted() {
+	const input = document.getElementById('input').value;
+  const nodes = parse(input);
+  // const pretty = nodes.map((node) => node.token).join('\n');
+  // outputElement.insertAdjacentText('beforeend', `${pretty}\n`);
+  // outputElement.scrollTop = outputElement.scrollHeight;
+
+  Array.from(richOutputElement.children)
+  .sort((left, right) => right.style.order - left.style.order) // descending
+  .forEach((child) => {
+    child.style.order = +child.style.order+1;
+  });
+
+  const fragment = createAnalysisFragment(nodes);
+  richOutputElement.prepend(fragment);
+}
+
+richOutputElement.addEventListener('click', function(event) {
+  event.stopPropagation();
+  if (new URLSearchParams(window.location.search).get('jisho')) {
+    handleTokenClickJishoIframe(event);
+  } else {
+    handleTokenClickEdict2(event);
+  }
+});
+
+const {
+  wrapped,
+  currentPointers,
+} = window;
+
+function parse(sentence) {
+  const whitespaces = [];
+  const r = new RegExp('[\\s　]+', 'g');
+  let match;
+  while(match = r.exec(sentence)) {
+    /*
+    0: ' ',
+    index: 27,
+    */
+    whitespaces.push(match);
+  }
+  const mecabOutput = wrapped.mecab_sparse_tostr(currentPointers.tagger, sentence);
+  console.log(mecabOutput);
+  const mecabTokens = toMecabTokens(mecabOutput);
+  const plusOriginalWhitespaces = withWhitespacesSplicedBackIn(mecabTokens, whitespaces);
+  const plusInterWordWhitespaces = withInterWordWhitespaces(plusOriginalWhitespaces);
+  return plusInterWordWhitespaces;
+}
+
 export {
+	handleFormSubmitted,
 	handleTokenClickEdict2,
 };
